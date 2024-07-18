4a47f6a16ac9cddf5d995f21fee65cd8fe985d54
Edgar <ejgallegos@espe.edu.ec>
2024-07-17 22:05:53 -0500

    style: Update title font size in Home page and fix typo in index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ export default function Home() {
     return (
         <div className={styles.container}>
             <main className={styles.main}>
-                <h1 className={styles.title}>Pokémons</h1>
+                <h1 className={styles.title}>POKÉMONS</h1>
                 <div className={styles.grid}>
                     {pokemons.map(pokemon => (
                         <div key={pokemon.id} className={styles.card}>
